09ecb3068a8000b10cfc64950c35987fce7197d8
AmirHossein Zare <zare20212021@gmail.com>
2025-01-26 13:03:14 +0330

    Add files via upload

---
+++ Learning Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Learning_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
